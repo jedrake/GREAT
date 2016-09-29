632ba31b6aa250596c31693c7642bda078d46ed7
jedrake <je.drake@uws.edu.au>
2016-09-29 13:28:21 +1000

    Switched to pdf()

--- R/compare GREAT and S30.R
+++ R/compare GREAT and S30.R
@@ -79,39 +79,42 @@ dat.s30.n$treat <- factor(substr(as.character(dat.s30.n$combotrt),start=1,stop=1
 #-----------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------
 #- plot normalized data relative to the growth temperature
-
-windows(40,50);par(mar=c(6,8,1,1),mfrow=c(1,1),cex.lab=1.5,cex.axis=1.2,oma=c(2,0,0,0))
+pdf(file="output/Figure7-Compare_experiments.pdf",width=3.5,height=4)
+#windows(40,50)
+par(mar=c(3,5,1,1),mfrow=c(1,1),cex.lab=1.3,cex.axis=1,oma=c(1,1,0,0))
 palette(c("red","green","blue"))
 palette(c("red","green","blue",rev(brewer.pal(11,"Spectral"))))
 
 #- plot the GREAT data
 plot(totdm_norm.mean~Tair,data=dat.gr.n,type="o",axes=F,xlab="",ylab="",pch=16,col="black",
-     ylim=c(0,1.2),cex=2,legend=F)
+     ylim=c(0,1.2),cex=1.5,legend=F)
 adderrorbars(x=dat.gr.n$Tair,y=dat.gr.n$totdm_norm.mean,
                               SE=dat.gr.n$totdm_norm.standard.error,direction="updown")
-magaxis(side=c(1,2),labels=c(1,1),las=1,frame.plot=T)
+magaxis(side=c(1,2,4),labels=c(1,1,0),las=1,frame.plot=T,las=1,ratio=0.4,tcl=0.2)
 
 #- overlay the S30 data
 adderrorbars(x=dat.s30.n$Tair,y=dat.s30.n$totdm_norm.mean,
              SE=dat.s30.n$totdm_norm.standard.error,direction="updown")
 plotBy(totdm_norm.mean~Tair|combotrt,data=dat.s30.n,type="o",axes=F,xlab="",ylab="",pch=21,col=c("black"),
-     ylim=c(0,1),cex=2,add=T,legend=F)
+     ylim=c(0,1),cex=1.5,add=T,legend=F)
 palette(c("blue","red"))
-points(totdm_norm.mean~Tair,data=dat.s30.n,add=T,pch=21,cex=2,legend=F,col="black",bg=treat)
+points(totdm_norm.mean~Tair,data=dat.s30.n,add=T,pch=21,cex=1.5,legend=F,col="black",bg=treat)
 
 arrows(x0=dat.s30.n$Tair[c(1,2,4,6)],y0=dat.s30.n$totdm_norm.mean[c(1,2,4,6)],
        x1=dat.s30.n$Tair[c(3,5,7,8)],y1=dat.s30.n$totdm_norm.mean[c(3,5,7,8)],
        code=2,lty=2)
 
 
-title(xlab=expression(Growth~T[air]~(degree*C)),cex.lab=2,line=4)
-title(ylab=expression(Final~total~mass~(normalized)),cex.lab=2,line=4)
+title(xlab=expression(Growth~T[air]~(degree*C)),cex.lab=1.3,line=2)
+title(ylab=expression(atop(Final~total~mass,
+                           ~(normalized))),cex.lab=1.3,line=2)
 
 legend("bottomright",legend=c("This study",expression(Drake~italic(et~al.)~(2015)~Home),
                               expression(Drake~italic(et~al.)~(2015)~Warmed)),pch=c(21,21,21)
-       ,col="black",cex=1.5,pt.bg=c("black","blue","red"))
+       ,col="black",cex=0.7,pt.bg=c("black","blue","red"))
 
-dev.copy2pdf(file="output/Figure7-experiment_comparison.pdf")
+dev.off()
+#dev.copy2pdf(file="output/Figure7-experiment_comparison.pdf")
 #-----------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------
 
